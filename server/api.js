001c4fd2e598fd5c7f3aedb59513ca7e32f0c2a1
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-10-23 11:22:06 -0400

    switching to new branch to add UI features

--- server/api.js
+++ server/api.js
@@ -1,11 +1,7 @@
 'use strict';
 const api = require('express').Router();
-// const db = require('../db')
 
-// If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
-	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
-	// Ideally you would have something to handle this, so if you have time try that out!
 api.use('/campus', require('./campus'));
-api.use('/student', require('./students'));
+api.use('/student', require('./student'));
 
 module.exports = api;
